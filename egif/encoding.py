18f9a9d86eb1664dd25f22e9dd775e9695c9bc07
Andre Fernandes <fpf.andre@gmail.com>
2021-01-08 18:43:21 -0300

    rewrite rle method

--- egif/encoding.py
+++ egif/encoding.py
@@ -1,62 +1,34 @@
-def encode(array):
-    counter = 0
+def run_length_encode(array):
+    # code to remove repetitions of the number 0
+    repeted = 0
     encoded = []
 
-    for n in array:
-        if not 0 <= n <= 255:
-            print(n)
-            raise ValueError('Values should be between 0 and 255')
-
-        if n == 0:
-            counter += 1
-        elif counter:
-            while counter > 0:
-                encoded.append(0)
-                cu = 255 if counter >= 255 else counter
-                encoded.append(cu)
-                counter -= cu
-            encoded.append(n)
+    for i in array:
+        if i == 0:
+            repeted += 1
+        elif repeted:
+            encoded.append(0)
+            encoded.append(repeted)
+            encoded.append(i)
+            repeted = 0
         else:
-            encoded.append(n)
-    
-    while counter > 0:
+            encoded.append(i)
+
+    if repeted:
         encoded.append(0)
-        cu = 255 if counter >= 255 else counter
-        encoded.append(cu)
-        counter -= cu
+        encoded.append(repeted)
+    return encoded
 
-    return bytes(encoded)
-
-def decode(array):
-    marker = False
+def run_length_decode(array):
+    last_was_zero = False
     decoded = []
 
-    cu = []
-
-    for n in array:
-        if n == 0:
-            marker = True
-        elif marker:
-            decoded.extend([0] * n)
-            cu.append(n)
-            marker = False
+    for i in array:
+        if i == 0:
+            last_was_zero = True
+        elif last_was_zero:
+            decoded.extend([0] * i)
+            last_was_zero = False
         else:
-            decoded.append(n)
-
-    print('Decode')
-    print(len(cu))
-    print(min(cu))
-    print(max(cu))
-    print(sum(cu) / len(cu))
-
+            decoded.append(i)
     return decoded
-
-
-# import numpy as np 
-
-# for i in range(1, 100):
-#     array = np.random.randint(0,255, size=i) * np.random.randint(0,2,size=i)
-#     a = encode(array)
-#     b = decode(a)
-#     assert np.alltrue(array == b)
-#     print('all right')
