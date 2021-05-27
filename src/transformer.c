fd2aafbb18a4493c386cef5757c18759d02d41f2
Andre Fernandes <fpf.andre@gmail.com>
2021-05-27 13:27:41 -0300

    solve bugs and implement dwt 2d

--- src/transformer.c
+++ src/transformer.c
@@ -5,7 +5,8 @@
 
 void dwt(int array[], int size, int levels, int steps) {
     int half;
-    int even, odd;
+    int even, odd; // even and odd indexes of the array 
+    int start, end; // first and second part of the array 
     int *temp = malloc(size * sizeof(int));
 
     for (int level = 0; level < levels; level++) {
@@ -22,8 +23,11 @@ void dwt(int array[], int size, int levels, int steps) {
 
         // Puts everything in the array using SIMD (I hope)
         for (int i = 0; i < half; i++) {
-            array[i] = temp[i];
-            array[i + half] = temp[i + half] - temp[i];
+            start = steps * i;
+            end   = steps * (i + half);
+
+            array[start] = temp[i];
+            array[end] = temp[i + half] - temp[i];
         }
     }
 
@@ -32,15 +36,18 @@ void dwt(int array[], int size, int levels, int steps) {
 
 void idwt(int array[], int size, int levels, int steps) {
     int half;
-    int even, odd;
+    int even, odd, start, end;
     int *temp = malloc(size * sizeof(int));
 
     for (int level = levels; level > 0; level--) {
         half = size >> level;
 
         for (int i = 0; i < half; i++) {
-            temp[i] = array[i];
-            temp[i + half] = array[i] + array[i+half];
+            start = steps * i;
+            end   = steps * (i + half);
+
+            temp[i] = array[start];
+            temp[i + half] = array[start] + array[end];
         }
 
         for (int i = 0; i < half; i++) {
@@ -53,4 +60,24 @@ void idwt(int array[], int size, int levels, int steps) {
     }
 
     free(temp);
+}
+
+void dwt_2d(int array[], int width, int height, int levels) {
+    for (int i = 0; i < height; i++) {
+        dwt(&array[i*width], width, levels, 1);
+    }
+
+    for (int i = 0; i < width; i++) {
+        dwt(&array[i], height, levels, width);
+    }
+}
+
+void idwt_2d(int array[], int width, int height, int levels) {
+    for (int i = 0; i < height; i++) {
+        idwt(&array[i*width], width, levels, 1);
+    }
+
+    for (int i = 0; i < width; i++) {
+        idwt(&array[i], height, levels, width);
+    }
 }
